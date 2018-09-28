f03baf10a032b9494f93cbc0081ab7117a396a0e
vyacheslav.petrov <vyacheslav.petrov@R22UF-OITGA-01.main.russianpost.ru>
2018-09-28 15:44:54 +0700

    Test start

---
+++ TestClasses/M_10_X0/Controller/CCurrentMoveControllerTest.java
@@ -0,0 +1,32 @@
+package M_10_X0.Controller;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import org.junit.Test;
+
+import M_10_X0.Model.CField;
+import M_10_X0.Model.EFigure;
+
+public class CCurrentMoveControllerTest {
+
+	@Test
+	public void testCurrentMove() throws Exception{
+//		fail("Not yet implemented");
+		final CField gameBoard = new CField(3);
+		
+		
+/*		for(int i=0; i<gameBoard.getSize(); i++) {
+			for(int j=0; j<gameBoard.getSize(); j++) {
+				if(Math.random() < 0.5) {
+					gameBoard.setFigure(new Point(i,j), EFigure.O);
+				} else {
+					gameBoard.setFigure(new Point(i,j), EFigure.X);
+				}
+			}
+		}
+*/
+	}
+
+}
