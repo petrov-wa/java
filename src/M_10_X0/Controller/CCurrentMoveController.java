6c3be099b23fe72c05116f3569298a95f6ed52b9
vyacheslav.petrov <vyacheslav.petrov@R22UF-OITGA-01.main.russianpost.ru>
2018-09-28 12:23:27 +0700

    add currentMove

--- src/M_10_X0/Controller/CCurrentMoveController.java
+++ src/M_10_X0/Controller/CCurrentMoveController.java
@@ -1,7 +1,10 @@
 package M_10_X0.Controller;
 
+import java.awt.Point;
+
 import M_10_X0.Model.CField;
 import M_10_X0.Model.EFigure;
+import M_10_X0.Model.Exceptions.InvalidPointException;
 
 public class CCurrentMoveController {
 
@@ -9,7 +12,18 @@ public class CCurrentMoveController {
 		// TODO Auto-generated constructor stub
 	}
 
-	EFigure currentMove(CField inField) {
-		return null; 
+	EFigure currentMove(CField gameBoard) throws InvalidPointException {
+		int cntX = 0;
+		int cnt0 = 0;
+		
+		for(int i=0; i<gameBoard.getSize(); i++){
+			for(int j=0; j<gameBoard.getSize(); j++) {
+				if(gameBoard.getFigure(new Point(i, j)) == EFigure.X) { cntX++; }
+				else if(gameBoard.getFigure(new Point(i,j)) == EFigure.O) { cnt0++; }
+			}
+		}	
+		
+		if(cnt0 < cntX) { return EFigure.O; }
+		else { return EFigure.X; }
 	}
 }
