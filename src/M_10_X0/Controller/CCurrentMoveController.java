8b6d2c773476469ca32f287e6470f85b9705d203
vyacheslav.petrov <vyacheslav.petrov@R22UF-OITGA-01.main.russianpost.ru>
2018-10-12 15:36:35 +0700

    CConsoleView: +move add logic in method +show : adjust view of game-board

--- src/M_10_X0/Controller/CCurrentMoveController.java
+++ src/M_10_X0/Controller/CCurrentMoveController.java
@@ -7,29 +7,32 @@ import M_10_X0.Model.EFigure;
 import M_10_X0.Model.Exceptions.InvalidPointException;
 
 public class CCurrentMoveController {
+    public CCurrentMoveController() {
+	super(); 
+    }
+    
+    public EFigure currentMove(CField gameBoard){
+	int cnt = 0;
 
-	public CCurrentMoveController() {
-		// TODO Auto-generated constructor stub
-	}
-
-	EFigure currentMove(CField gameBoard) throws InvalidPointException {
-/*		int cntX = 0;
-		int cnt0 = 0;
+	for(int i=0; i<gameBoard.getSize(); i++){
+	    for(int j=0; j<gameBoard.getSize(); j++) {
+/*		switch(gameBoard.getFigure(new Point(i,j))) {
+		case X: cntX++; break;
+		case O: cnt0++; break;
+		}
 */
-		int cnt = 0;  
-		
-		for(int i=0; i<gameBoard.getSize(); i++){
-			for(int j=0; j<gameBoard.getSize(); j++) {
-/*				switch(gameBoard.getFigure(new Point(i,j))) {
-					case X: cntX++; break;  
-					case O: cnt0++; break; 
-				}
-*/
-				if(gameBoard.getFigure(new Point(i,j)) != null) { cnt++; }
-			}
-		}	
-		if(cnt == gameBoard.getSize()*gameBoard.getSize()) { return null; }
-		if(cnt % 2 == 1) { return EFigure.O; }
-		else { return EFigure.X; }
+		try {
+		    if(gameBoard.getFigure(new Point(i,j)) != null) { cnt++; }
+		}catch(InvalidPointException e) {
+		    /*
+		     * if get exception via simplify walking on game board -- then game mast fall   
+		     * */
+		    throw new RuntimeException(e); 
+		}
+	    }
 	}
+	if(cnt == gameBoard.getSize()*gameBoard.getSize()) { return null; }
+	if(cnt % 2 == 1) { return EFigure.O; }
+	else { return EFigure.X; }
+    }
 }
--- src/M_10_X0/View/CConsoleView.java
+++ src/M_10_X0/View/CConsoleView.java
@@ -1,55 +1,92 @@
 package M_10_X0.View;
 
 import java.awt.Point;
+import java.util.Scanner;
 
+import M_10_X0.Controller.CCurrentMoveController;
+import M_10_X0.Controller.CWinnerController;
 import M_10_X0.Model.CField;
 import M_10_X0.Model.CGame;
 import M_10_X0.Model.EFigure;
 import M_10_X0.Model.Exceptions.InvalidPointException;
+import M_10_X0.Model.Exceptions.NoWinnerException;
 
 public class CConsoleView {
-	
-	public void show(final CGame game) {
-		final CField gameBoard = game.getField();
-		
-		System.out.format("Game name: %s\n",  game.getName());
-		
-		for(int row=0; row<gameBoard.getSize(); row++) {
-			printSepartor(gameBoard); 
-			printBoardLine(gameBoard, row);
-		}
+
+    private final CCurrentMoveController currentMoveController = new CCurrentMoveController();
+    private final CWinnerController winnerController = new CWinnerController(); 
+    
+    public void show(final CGame game) {
+	final CField gameBoard = game.getField();
+	System.out.format("Game name: %s\n",  game.getName());
+
+	for(int row=0; row<gameBoard.getSize(); row++) {
+	    if(row>0) {
 		printSepartor(gameBoard); 
-	}
-
-
-	public void move(final CGame game) { 
-		
-	}
-
-	private void printBoardLine(final CField gameBoard, final int row){
-		
-		for(int column=0; column<gameBoard.getSize(); column++) {
-			final EFigure figure ;
-			try {
-				figure = gameBoard.getFigure(new Point(row, column));
-			} catch(InvalidPointException e) {
-				e.printStackTrace();
-				throw new RuntimeException(e); 
-			}
-			
-			System.out.format("| %s ", (figure != null? figure : " ")); 
-//			System.out.print("|");
-//			System.out.print(figure != null? figure : " ");
-		}
-		System.out.println("|");
-	}
-	
-
-
-	private void printSepartor(final CField gameBoard) {
-	    for(int i=0; i<gameBoard.getSize(); i++) {
-		System.out.print("+---");
 	    }
-	    System.out.println("+");
+	    printBoardLine(gameBoard, row);
 	}
+//		printSepartor(gameBoard); 
+    }
+
+    public boolean move(final CGame game) {
+	final CField gameBoard = game.getField(); 
+	final EFigure currentFigure = this.currentMoveController.currentMove(gameBoard );
+	if(this.currentMoveController == null) {
+	    try {
+		final EFigure winner = this.winnerController.getWinner(gameBoard);
+		System.out.format("We got a winner: %s. Game over.", winner);
+	    } catch (NoWinnerException e) {
+		/*
+		 * No winner 
+		 * */
+		System.out.println("No winner and no more turns left! Game over.");
+	    }
+	    return false; 
+	}
+	System.out.format("Enter turn point for player %s", currentFigure);
+	final Point point = askPoint();  
+
+	return true; 
+    }
+
+    private Point askPoint() {
+	return new Point(askCoordinate("row"), askCoordinate("column"));  
+    }
+
+    private int askCoordinate(final String coordinateName) {
+	System.out.format("Please, enter %s:", coordinateName);
+	Scanner in= new Scanner(System.in);
+	return in.nextInt(); 
+    }
+
+    private void printBoardLine(final CField gameBoard, final int row){
+
+	for(int column=0; column<gameBoard.getSize(); column++) {
+	    final EFigure figure ;
+	    try {
+		figure = gameBoard.getFigure(new Point(row, column));
+	    } catch(InvalidPointException e) {
+		e.printStackTrace();
+		throw new RuntimeException(e);
+	    }
+	    System.out.format("%s %s ", (column>0?"|":""), (figure != null? figure : " ")); 
+//	    System.out.print("|");
+//	    System.out.print(figure != null? figure : " ");
+//	    try {
+//		
+//	    }
+	}
+	System.out.println();
+    }
+
+    private void printSepartor(final CField gameBoard) {
+	for(int i=0; i<gameBoard.getSize(); i++) {
+	    if(i!=0) {
+		System.out.print("+");
+	    }
+	    System.out.print("---");
+	}
+	System.out.println();
+    }
 }
